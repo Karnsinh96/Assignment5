3ee6c8a32e7dbdede2e439bd37823689936ec94a
Karnsinh96 <91832296+Karnsinh96@users.noreply.github.com>
2021-10-31 21:14:46 +0530

    Add files via upload

---
+++ Find_power.py
@@ -0,0 +1,13 @@
+class Find_Power:
+    
+    def power(self):
+        self.x=x
+        self.n=n
+        a=x**n
+        print("The",n,"th Power of ",x," is ",a)
+       
+x=int(input("Enter The Number:- "))
+n=int(input("Enter The Power:- "))
+
+fp=Find_Power()
+fp.power()
